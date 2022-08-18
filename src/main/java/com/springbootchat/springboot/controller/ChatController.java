b4331363927f66fbed31b3cf4b55c11417d0ec4c
Helen Isu <helenisu0@gmail.com>
2022-08-18 13:04:31 +0100

    chat app cretaed with springboot

---
+++ src/main/java/com/springbootchat/springboot/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.springbootchat.springboot.controller;
+
+
+import com.springbootchat.springboot.models.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessagePojo) {
+        return chatMessagePojo;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessagePojo, SimpMessageHeaderAccessor headerAccessor) {
+
+// Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessagePojo.getSender());
+        return chatMessagePojo;
+    }
+
+}
